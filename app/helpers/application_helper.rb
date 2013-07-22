f3ce6120f50ee2ba9b8e71a57c615541262f488f
mthahakp <mthahakp@gmail.com>
2013-07-22 10:21:34 +0530

    caching user index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
-
-
-def sortable(column, title = nil)
-  search=params[:search]
-  title ||= column.titleize
-  css_class = column == sort_column ? "current #{sort_direction}" : nil
-  direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-  link_to title, {search: search, :sort => column, :direction => direction}, {:class => css_class}
-end
-
-
-
+  def sortable(column, title = nil)
+    search=params[:search]
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {search: search, :sort => column, :direction => direction}, {:class => css_class}
+  end
+  def pagination_links(collection, options = {})
+    options[:renderer] ||= BootstrapPaginationHelper::LinkRenderer
+    options[:class] ||= 'pagination pagination-centered'
+    options[:inner_window] ||= 2
+    options[:outer_window] ||= 1
+    will_paginate(collection, options)
+  end
 end
