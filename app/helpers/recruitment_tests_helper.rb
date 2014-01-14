d425d559f4723386b096cac6bb4a5ab994100690
mthahakp <mthahakp@gmail.com>
2014-01-14 16:25:55 +0530

    question

--- app/helpers/recruitment_tests_helper.rb
+++ app/helpers/recruitment_tests_helper.rb
@@ -2,7 +2,8 @@ module RecruitmentTestsHelper
   def find_extra(recruitment_test)
     @candidate = recruitment_test.candidate
     questions = @candidate.schedule.exam.question_ids
+    extra_questions = Category.where("category = 'Additional'").first.question_ids
     @answers =  @candidate.answers.where("question_id in (?)",questions)
-    @extra_answers = @candidate.answers.where("question_id not in (?)",questions)
+    @extra_answers = @candidate.answers.where("question_id in (?)",extra_questions)
   end
 end
