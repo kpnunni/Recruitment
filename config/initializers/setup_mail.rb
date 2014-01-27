789531e2f5ab354c42d132c1d2d25c15c17f5700
mthahakp <mthahakp@gmail.com>
2014-01-27 14:55:27 +0530

    question

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
       :port                 => 587,  
       :domain               => "gmail.com",  
       :user_name            => "suyationlinetest", #Your user name
-      :password             => "Suyati123", #
+      :password             => "suyati1234", #
       :authentication       => "plain",
       :enable_starttls_auto => true
    }
